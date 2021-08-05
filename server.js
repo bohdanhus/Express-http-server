76464c2fef9c4911709dc86f8b12092d1e7115cb
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-08-05 09:43:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const url = require('url')
 const http = require(`http`);
-
-
+const { url } = require("inspector");
 
 function getWordCounterReduce(testText) {
     let words = testText.toLowerCase().replace(/[.,\s]/g, ' ').split(' ').filter(String);
-    console.log(words);
     return words.reduce((prev, next) => {
         prev[next] = (prev[next] + 1) || 1;
         return prev;
@@ -29,62 +26,63 @@ function Plural(num, nom, gen, plu) {
     return;
 };
 
-
-// const myObject = url.parse("localhost:3000/plural?number=2&forms=person,people,people", true).query; // { number : '2', forms: 'preson,people,people}
-//const string = '/plural?number=2&forms=person,people,people'
-//let paramth = string.split('?');
-//console.log(paramth[1])
-
-
 const server = http.createServer((req, res) => {
 
     const reqUrl = req.url.split('?');
+
     const pathname = reqUrl[0];
+    
     const query = reqUrl[1];
 
     if (pathname === '/headers') {
+
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end((JSON.stringify(req.headers) + '\n'));
         // curl 'localhost:3000/headers' 
+    }
+    if (pathname === '/plural') {
 
-    } else if (pathname === '/plural') {
         const searchParams = new URLSearchParams(query);
+
         const newforms = searchParams.get('forms').split(",");
+
         res.writeHead(200, { 'Content-type': 'text/plan' });
-        res.end(Plural(`${searchParams.get('number') - 1}`, `${newforms[0]}`, `${newforms[1]}`, `${newforms[2]}`) + '\n');
+        res.end(Plural(`${searchParams.get('number')} `,`${newforms[0]}`,`${newforms[1]}`,`${newforms[2]}`) + '\n');
+       
         // curl 'localhost:3000/plural?number=2&forms=person,people,people'
 
-    } else if (pathname === '/frequency' && req.method === 'POST') {
-        // curl -X POST localhost:5000/frequency --data-raw "Little red fox jumps over logs. Fox is red"
-        // добавить в ответ 3 заголовка: Content-Type: application/json; количество уникальных слов; самое частое слово.
-        let data = "";
-        req.on("data", chunk => data += chunk
-        )
-        req.on("end", () => {
-            let greatValue = 0;
-            let keyValue = {};
-            const input = getWordCounterReduce(data);
-            for (let key in input) { //let input = {"fox": 2 ,"is": 1, "jumps": 1, "little": 1,  "logs": 1,  "over": 1,   "red": 2 };
-                let next = input[key];
-                if (greatValue < next) {
-                    greatValue = next;
-                    keyValue = key; //console.log(keyValue); // "fox"
-                }
-            }
-
-            let count = Object.keys(input).length;
-            res.writeHead(200, { 'Content-type': 'application/json' });
-            res.setHeader("Word count", `${count}`);
-            res.setHeader("Most popular word", `${keyValue}`);
-            
-            res.end(JSON.stringify(input))
-        });
     }
+    if (pathname === '/frequency') {
 
-    res.writeHead(200, { 404: 'Not found' });
+            let data = "";
+            req.on("data", chunk => {
+                data += chunk
+            });
+            req.on("end", () => {
+                const result = getWordCounterReduce(data)
+                console.log(result)
 
-    res.end();
+                let counter = 0;
+                let keyval = {};
 
+                for (let key in result) {
+                    let next = result[key];
+                    if (counter < next) {
+                        counter = next;
+                        keyval = key; // "red"
+                        console.log(keyval); // "fox"
+                    }
+                }
+
+                res.setHeader("Most-popular", keyval )
+                res.setHeader("Words-counter", Object.keys(result).length)
+                res.writeHead(201, { 'Content-type': 'application/json' })
+
+                res.end((JSON.stringify(result)))
+            }); // curl localhost:3000/frequency -d "Little red fox jumps over logs. Fox is red"
+        } else {
+        res.writeHead(404, 'Not found').end();
+    }
 })
 
 const port = 3000
